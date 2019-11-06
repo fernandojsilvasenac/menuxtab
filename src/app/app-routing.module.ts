f505dc77e114391cbfc5021f3c06edefe2fe943d
unknown <informatica@note-fernando.feagri.unicamp.br>
2019-11-06 00:11:57 -0200

    inserção das tabs/pages, rotas e menu acessando as tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 
